4b3e0a12f2ba4f34f94d6c179036a80393cc1363
EthanCantor <ethan.cantor99@gmail.com>
2024-08-14 00:17:53 -0400

    is mobile fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,39 +2,40 @@ import type { Metadata } from "next";
 import "./globals.css";
 import SidebarCard from "@/components/Sidebar";
 import { BODY_FONT } from "@/fonts";
-import { useWidth } from "@/hooks/use-width";
-import { useRef } from "react";
 import { isMobileDevice } from "@/utils/isMobile";
+import { MobileNavbar } from "@/components/MobileNavbar";
 
 export const metadata: Metadata = {
   title: "Resume Ethan Cantor",
   description: "Ethan Cantor's very cool resume",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
 
-  const isMobile = isMobileDevice();
-
+  const isMobile = await isMobileDevice();
 
   return (
     <html lang="en">
       <body className={BODY_FONT.className + ' flex flex-row'}>
         {isMobile
         ? 
-          <div className="overflow-x-clip">
-            {children}
-          </div>
+          <>
+            <MobileNavbar />
+            <div className="overflow-x-clip">
+              {children}
+            </div>
+          </>
         :
-            <>            
-              <SidebarCard />
-              <div className="ms-52 w-[calc(100vw-13rem)] overflow-x-clip">
-                {children}
-              </div>
-            </>
+          <>            
+            <SidebarCard />
+            <div className="ms-52 w-[calc(100vw-13rem)] overflow-x-clip">
+              {children}
+            </div>
+          </>
         }
       </body>
     </html>
---
+++ src/components/MobileNavbar.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React, { useState } from 'react'
+
+export const MobileNavbar = () => {
+    const [expanded, setExpanded] = useState();
+    return (
+        <div className='bg-raisin-dark'>
+            
+        </div>
+    )
+};
+
