1223b44f32f7d7a321120763a0b477d2f02c3760
EthanCantor <ethan.cantor99@gmail.com>
2024-08-13 22:01:05 -0400

    remove extra import

--- src/components/AboutMePage.tsx
+++ src/components/AboutMePage.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import MusicBackground from './MusicBackground'
 import { TITLE_FONT } from '@/fonts'
 
 export const AboutMePage = () => {
 
     return (
-        <div className='h-screen w-screen flex flex-col justify-center items-center p-32 space-y-10 relative'>
-            <div className='absolute'>
+        <div className='h-screen w-screen flex flex-col justify-center items-center p-32 space-y-10'>
+            <div className=''>
                 <div className={'text-6xl ' + TITLE_FONT.className}>Ethan Cantor</div>
                 <div className='text-xl'>A full stack engineer out of Philadelphia, PA</div>
             </div>
