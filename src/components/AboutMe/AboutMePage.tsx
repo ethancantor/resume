87847bab6e404970b1cd194bd8eead07d7cefe1a
EthanCantor <ethan.cantor99@gmail.com>
2024-08-17 22:43:03 -0400

    finish about me page for mobile and desktop

--- src/components/AboutMe/AboutMePage.tsx
+++ src/components/AboutMe/AboutMePage.tsx
@@ -10,14 +10,14 @@ export const AboutMePage = () => {
 
     return (
         <div className='h-screen w-screen flex flex-col flex-shrink-0 flex-wrap space-x-5 justify-center items-center'>
-            <div className='flex flex-row flex-shrink-0 flex-wrap space-x-5 justify-center items-center p-32'>
+            <div className='flex flex-row flex-shrink-0 flex-wrap space-x-5 justify-center items-center'>
                 <Image className="mb-3 rounded-full drop-shadow-md border-4 border-primary object-fill aspect-square max-h-56 max-w-56" src={Avatar} alt="Ethan image" unoptimized/>        
-                <div>
+                <div className='max-w-[75%] mx-auto'>
                     <div className={`text-6xl ${TITLE_FONT.className}`}>Ethan Cantor</div>
                     <div className='text-xl'>A full stack engineer out of Philadelphia, PA</div>
-                    <div className='flex flex-row flex-wrap space-x-5'>
-                        <Link href='/files/Ethan_Cantor_Resume.pdf' className='text-blue-400'>Resume</Link>
-                        <Link href='/music' className='text-blue-400'>Music Portfolio</Link>
+                    <div className='flex flex-row flex-wrap space-x-5 text-blue-400 underline'>
+                        <Link href='/files/Ethan_Cantor_Resume.pdf'>Resume</Link>
+                        <Link href='/music'>Music Portfolio</Link>
                     </div>
                 </div>
             </div>
