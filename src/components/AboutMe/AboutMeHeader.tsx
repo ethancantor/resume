fedb7d3167178336d1ec4c90010e5b77e7538478
ethancantorescapemusic <ethan.cantor99@gmail.com>
2024-08-21 13:30:33 -0400

    fixed mobile icons being too big

--- src/components/AboutMe/AboutMeHeader.tsx
+++ src/components/AboutMe/AboutMeHeader.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import { useScrollTo } from '@/hooks/use-scroll'
 import { motion, useInView,  } from 'framer-motion'
 import { useWidth } from '@/hooks/use-width';
+import { HeaderHexIcon } from '../Misc/HeaderHexIcon';
 
 export const AboutMeHeader = () => {
 
@@ -19,6 +20,7 @@ export const AboutMeHeader = () => {
         hide: { x: -100, opacity: 0 },
         show: { x: 0, opacity: 100 }
     }
+
 
     return (
         <>
@@ -37,16 +39,16 @@ export const AboutMeHeader = () => {
             <motion.div className={`flex ${width < 500 ? 'flex-row w-full' : 'flex-col'} flex-wrap h-fit z-20 fixed top-0 left-0  justify-center border-primary border-2 bg-raisin-dark rounded-2xl`}
                 variants={variants} animate={isInView ? 'hide' : 'show'} initial={'hide'}  
             >
-                <HexIcon icon={BackToTopIcon} name='Top' onClick={() => scrollToSection('about-me-section')} />
-                <HexIcon icon={SkillsIcon} name='Skills' onClick={() => scrollToSection('skills-section')}/>
-                <HexIcon icon={ProjectsIcon} name='Projects' onClick={() => scrollToSection('projects-section')}/>
-                <HexIcon icon={ExperienceIcon} name='Experince' onClick={() => scrollToSection('experience-section')}/>
-                <HexIcon icon={MusicIcon} name='Music' onClick={() => scrollToSection('music-section')}/>
+                <HeaderHexIcon icon={BackToTopIcon} name='Top' onClick={() => scrollToSection('about-me-section')} isMobile={width < 500}/>
+                <HeaderHexIcon icon={SkillsIcon} name='Skills' onClick={() => scrollToSection('skills-section')} isMobile={width < 500}/>
+                <HeaderHexIcon icon={ProjectsIcon} name='Projects' onClick={() => scrollToSection('projects-section')}  isMobile={width < 500}/>
+                <HeaderHexIcon icon={ExperienceIcon} name='Experince' onClick={() => scrollToSection('experience-section')} isMobile={width < 500}/>
+                <HeaderHexIcon icon={MusicIcon} name='Music' onClick={() => scrollToSection('music-section')} isMobile={width < 500}/>
                 <Link href={'mailto:ethan.cantor99@gmail.com'}>
-                    <HexIcon icon={EmailIcon} name='Email Me'/>
+                    <HeaderHexIcon icon={EmailIcon} name='Email Me' isMobile={width < 500}/>
                 </Link>
                 <Link href={'/files/Ethan_Cantor_Resume.pdf'}>
-                    <HexIcon icon={ResumeIcon} name='Resume'/>
+                    <HeaderHexIcon icon={ResumeIcon} name='Resume' isMobile={width < 500}/>
                 </Link>
             </motion.div>
         </>
